73c5ad002f53032a2fdbd60d303bee96b19c4634
yuminK <helloaiworld@gmail.com>
2025-07-15 20:44:03 +0900

    Update 071525_LC_CanPlaceFlowers.py
    2nd fail

--- 071525_LC_CanPlaceFlowers.py
+++ 071525_LC_CanPlaceFlowers.py
@@ -1,14 +1,15 @@
 def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
     i = 0 
+    print(len(flowerbed))
     
-    while i < (len(flowerbed) - 2)  :
+    while i < (len(flowerbed))  :
         print("i: ", i)
         print("n: ", n)
         if flowerbed[i] == 1 : 
             i = i+2 
             
         else :
-            if flowerbed[i+1] == 0 :
+            if (i+1 >= len(flowerbed)) or (flowerbed[i+1] == 0):
                 n = n - 1 
                 i = i + 2 
             else :
@@ -27,3 +28,8 @@ if __name__ == '__main__' :
     
     results = canPlaceFlowers(flowerbed, n)
     print(results)
+    
+# 1. [1,0,0,0,1,0,0], n=2 처럼 심을 수 있는 공간이 맨 마지막에 있는 경우를 고려하지 않았음 
+#    => while문의 조건을 len(flowerbed)로 변경하여 끝까지 돌되, (i+1 >= len(flowerbed))를 추가하여 맨 끝인지 아닌지 확인하고자 함
+# 2. 우려하던 list index out of range 발생
+#    => 첫 번째 if문에서 발생 
