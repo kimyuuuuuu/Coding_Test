3f4d803c7188c2a15c22b4e766fda3b9d5ca8448
yuminK <helloaiworld@gmail.com>
2025-07-15 21:31:08 +0900

    Update 071525_LC_CanPlaceFlowers.py
    accept

--- 071525_LC_CanPlaceFlowers.py
+++ 071525_LC_CanPlaceFlowers.py
@@ -1,26 +1,21 @@
 def canPlaceFlowers(flowerbed: list[int], n: int) -> bool:
-    i = 0 
-    # print(len(flowerbed))
+    if n== 0 :
+        return True 
     
-    while i < (len(flowerbed))  :
-        print("i: ", i)
-        print("n: ", n)
-        if flowerbed[i] == 1 : 
-            i = i+2 
+    for i in range(len(flowerbed)) : 
+        if flowerbed[i] == 0 :
+            left_check = (i==0) or (flowerbed[i-1]==0)
+            right_check = (i==len(flowerbed)-1) or (flowerbed[i+1]==0)
             
-        else :
-            if (i-1 < 0 or (flowerbed[i-1] == 0)) and ((i+1 >= len(flowerbed)) or (flowerbed[i+1] == 0)):
-                n = n - 1 
-                i = i + 2 
-            else :
-                i = i + 2 
+            if left_check and right_check :
+                flowerbed[i] = 1 
+                n -= 1 
+                if n == 0 :
+                    return True 
                 
-    print("result n : ", n)
-    if n <= 0 :
-        return True
+    return False
     
-    else : 
-        return False
+    
     
 if __name__ == '__main__' : 
     flowerbed = list(map(int, input().split(',')))
@@ -36,4 +31,7 @@ if __name__ == '__main__' :
 # 3. [0,1,0] 과 같은 상황에서, 마지막이 0일 때 왼쪽을 체크하지 않아 문제 발생
 #    => (flowerbed[i-1] == 0) 를 추가하여 왼쪽 확인 
 # 4. [0,0,1,0,0] n=1 에서 심을 수 있는 곳이 2군데 있어서 n이 -1이 됨 .. 
-#    => n이 0이 되면 return 하도록 
+#    => n이 0이 보다 작으면 return true
+# 5. [0,1,0,1,0,1,0,0]에서 n=1일 때 ... flowerbed가 짝수인 것만 생각하고 코드를 짬.. 홀수도 고려했어야 함
+#    => 코드 전면 수정 ..... 
+# 6. n=0인 경우 확인 .......해야함.......
