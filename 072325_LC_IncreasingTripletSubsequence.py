d694237f94b53c388572b3d827cd4a311804f98e
yuminK <helloaiworld@gmail.com>
2025-07-23 22:10:01 +0900

    Update 072325_LC_IncreasingTripletSubsequence.py
    1st fail

--- 072325_LC_IncreasingTripletSubsequence.py
+++ 072325_LC_IncreasingTripletSubsequence.py
@@ -1,5 +1,28 @@
-def increasingTriplet(self, nums: list[int]) -> bool:
-    nums 
+def increasingTriplet(nums: list[int]) -> bool:
+    result = 1
+    current_num = nums[0]
+    
+    for i, num in enumerate(nums) :
+        if current_num >= num :
+            result = 1
+            
+        else : 
+            result += 1
+            
+        if result == 3 :
+            return True 
+        
+        print(result, current_num, num)
+            
+        current_num = num 
+        
+            
+    return False
+            
     
 if __name__ == '__main__' :
     s = list(map(int, input().split(',')))
+    
+    print(increasingTriplet(s))
+    
+# 1. 문제 이해 실수 .. 연속되지 않아도 됨 (index가 작기만 하면 됨, i < j < k)
