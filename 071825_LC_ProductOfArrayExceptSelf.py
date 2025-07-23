d00cd7b47a9f76595a254b8cf86f64d132ccf42d
yuminK <helloaiworld@gmail.com>
2025-07-23 21:51:53 +0900

    Update 071825_LC_ProductOfArrayExceptSelf.py
    accept

--- 071825_LC_ProductOfArrayExceptSelf.py
+++ 071825_LC_ProductOfArrayExceptSelf.py
@@ -1,21 +1,23 @@
 def productExceptSelf(nums: list[int]) -> list[int] :
     result = [] 
-    zero = False
-    j = nums[0]
+    zero = 0
+    j = 1
     
-    for i in range(len(nums)-1) :
-        if nums[i+1] == 0 :
-            zero = True
+    for i in range(len(nums)) :
+        if nums[i] == 0 :
+            zero += 1
             continue 
-        j = nums[i+1]*j
+        j = nums[i]*j
     
-        
-    if zero : 
+    if zero == 1: 
         for i in nums :
             if i != 0 :
                 result.append(0)
             else :
                 result.append(j)
+    
+    elif zero >= 1 :
+        result.extend([0] * len(nums))
                 
     else :
         for i in nums : 
@@ -30,4 +32,5 @@ if __name__ == '__main__' :
     print(results)
 
 # 1. [0,0]의 case를 생각 못 함 -> j = nums[0]으로 둠
-# 2. 여러 수가 들어간 리스트에서 0이 여러 개인 경우 생각 못함 
+# 2. 여러 수가 들어간 리스트에서 0이 여러 개인 경우 생각 못함 -> 0의 갯수에 따라 다른 과정
+# +) 처음 풀었을 때 보다 Runtime이 240ms 줄음. . .
