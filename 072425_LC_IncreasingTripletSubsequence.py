f09cef8ae72a0dd2eaa46ee777540a37e7b1cc48
yuminK <helloaiworld@gmail.com>
2025-07-23 22:14:58 +0900

    chage date

--- 072425_LC_IncreasingTripletSubsequence.py
+++ 072425_LC_IncreasingTripletSubsequence.py
@@ -1,21 +1,22 @@
 def increasingTriplet(nums: list[int]) -> bool:
     result = 1
+    result_idx = 0
     current_num = nums[0]
     
     for i, num in enumerate(nums) :
         if current_num >= num :
             result = 1
+            result_idx = i
             
-        else : 
+        elif current_num < num and result_idx < i : 
             result += 1
             
         if result == 3 :
             return True 
-        
-        print(result, current_num, num)
             
         current_num = num 
         
+        print(num, ':', result, current_num, result_idx)
             
     return False
             
