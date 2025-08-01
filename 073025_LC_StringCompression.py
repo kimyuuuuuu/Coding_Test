3f26d0b707e0262c4e7b544a102a8afccc532ec8
yuminK <helloaiworld@gmail.com>
2025-08-01 09:53:56 +0900

    accept

---
+++ 073025_LC_StringCompression.py
@@ -0,0 +1,32 @@
+
+def compress(chars: list[str]) -> int:
+    read = 0
+    write = 0
+    
+    while read < len(chars) :
+        current_char = chars[read]
+        count = 0 
+        
+        while read < len(chars) and chars[read] == current_char:
+            count += 1 
+            read += 1
+        
+        chars[write] = current_char
+        write += 1
+        
+        if count > 1 :
+            count_str = str(count) 
+            for digit in count_str :
+                chars[write] = digit 
+                write += 1
+                
+            
+    return write    
+
+if __name__ == '__main__' :
+    chars = list(map(int(input().split(','))))
+    
+    print(compress(chars))
+    
+    
+# two point 방식
