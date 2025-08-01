707eaaee941beeb019d0fa170ec39a89a9af1a06
yuminK <helloaiworld@gmail.com>
2025-08-01 23:24:12 +0900

    accept

--- 080125_LC_IsSubsequence.py
+++ 080125_LC_IsSubsequence.py
@@ -12,15 +12,16 @@ def isSubsequence(s: str, t: str) -> bool:
         current = s[read1] 
         
         while read2 < len(t) and t[read2] != current  :
-            read2 += 1 
-
-        read1 += 1
+            read2 += 1
+            
+        if read2 < len(t) :
+            read1 += 1
+            read2 += 1
+            
+        else :
+            break
         
-    if read1 == len(s) :
-        return True 
-        
-    else :
-        return False
+    return read1 == len(s)
     
 
 if __name__ == '__main__' :
@@ -29,4 +30,7 @@ if __name__ == '__main__' :
     
     print(isSubsequence(s, t))
     
-# "b", "c" 인 경우 
+# 1.  "b", "c" 인 경우 => 두 번째 while 문을 돌 수 없음 
+#      => 맨 처음에 한자리 문자열 걸러내도록 함
+# 2. 논리적 오류 발생
+#    => read1, 2를 언제 증가시켜야하는지 고민할 것
