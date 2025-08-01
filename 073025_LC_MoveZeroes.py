57ff8bac742263ec32e7a39a4c10d13f1299e50c
yuminK <helloaiworld@gmail.com>
2025-08-01 21:07:49 +0900

    accept

--- 073025_LC_MoveZeroes.py
+++ 073025_LC_MoveZeroes.py
@@ -1,13 +1,16 @@
 def moveZeroes(nums: list[int]) -> None:
-    count = 0
-    for i in range(len(nums)) :
-        if nums[i] == 0 :
-            count += 1 
-            del nums[i]
-            
-    nums.extend([0] * count)
+    write = 0
     
-
+    for i in range(len(nums)) :
+        if nums[i] != 0 :
+            nums[write] = nums[i]
+            write += 1
+            
+    while write < len(nums) :
+        nums[write] = 0
+        write += 1            
+        
+    
 if __name__ == '__main__' :
     chars = list(map(int(input().split(','))))
     
