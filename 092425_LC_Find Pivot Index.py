fde4af3a450781bb5d7157fbfb6a62afe429ed15
yuminK <helloaiworld@gmail.com>
2025-09-24 19:22:51 +0900

    accept

--- 092425_LC_Find Pivot Index.py
+++ 092425_LC_Find Pivot Index.py
@@ -1,6 +1,6 @@
 def pivotIndex(nums: list[int]) -> int:
     prefix = [0] * (len(nums)+1)
-    prefix_len = len(prefix)
+    prefix_len = len(prefix) -1
     
     # create prefix sum 
     for i in range(1, len(nums)+1):
@@ -8,7 +8,7 @@ def pivotIndex(nums: list[int]) -> int:
         
         
     for j in range(len(nums)) :
-        if prefix[0] == prefix[prefix_len-j] :
+        if prefix[j] == (prefix[prefix_len] - prefix[j+1]) :
             return j 
     
     return -1
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     
     print(pivotIndex(nums))
     
+# len(prefix) 하면 길이는 7인데 마지막 인덱스는 6인 경우들 발생 => out of range
+# 바보같이 빼기 안하고 인덱스 빼기 함 .... prefix[prefix_len-j]
